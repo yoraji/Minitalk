df3c19d44422ef512a7022e00ca4b55dd297f6cb
yoraji <rajiy8299@gmail.com>
2024-12-30 23:16:33 +0100

    MINITALK

---
+++ client_bonus.c
@@ -0,0 +1,3 @@
+#include "minitalk_bonus.h"
+
+
---
+++ minitalk_bonus.c
@@ -0,0 +1,23 @@
+#ifndef MINITALK_BONUS
+#define MINITALK_BONUS
+
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include "libft/ft_atoi.c"
+#include "printf/ft_printf.h"
+#include "printf/ft_printf.c"
+#include <stddef.h>
+
+#define YELLOW "\033[33m"
+#define RED "\033[31m"
+#define END "\033[0m"
+
+#include "printf/ft_putchar.c"
+#include "printf/ft_putnbr.c"
+#include "printf/ft_point.c"
+#include "printf/ft_hexdecimal.c"
+#include "printf/ft_unsigned.c"
+#include "printf/ft_putstr.c"
+
+#endif
